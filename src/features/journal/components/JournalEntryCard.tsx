dceccaf103e380185197fe326e1f7d874629107f
biervertrieb <biervertrieb@googlemail.com>
2025-08-16 23:02:26 +0200

    wip adding journalentries to timeline bit by bit...

---
+++ src/features/journal/components/JournalEntryCard.tsx
@@ -0,0 +1,13 @@
+import type { JournalEntry } from "../types";
+
+type Props = {
+    entry: JournalEntry
+}
+
+const JournalEntryCard = ({ entry }: Props) => {
+    return (
+        <div>Journal Entries coming soon...</div>
+    );
+}
+
+export default JournalEntryCard;
--- src/features/journal/components/JournalTimeline.tsx
+++ src/features/journal/components/JournalTimeline.tsx
@@ -1,6 +1,42 @@
+import type { JournalEntry } from "../types";
+import JournalEntryCard from "./JournalEntryCard";
+
+let entries: [JournalEntry];
+entries = [
+    {
+        "id": "1",
+        "title": "Entry 1",
+        "day": "1",
+        "body": "first entry",
+        "created_at": "whenever"
+    },
+    {
+
+        "id": "2",
+        "title": "Entry 2",
+        "day": "2",
+        "body": "second entry",
+        "created_at": "whenever"
+    },
+    {
+
+        "id": "3",
+        "title": "Entry 3",
+        "day": "3",
+        "body": "third entry",
+        "created_at": "whenever"
+    }
+]
+
 const JournalTimeline = () => {
     return (
-        <div>Timeline coming soon...</div>);
+        <div className="relative px-8">
+            <div className="absolute left-1/2 top-0 bottom-0 w-1 bg-gradient-to-b from-yellow-400 via-yellow-500 to-yellow-400 transform -translate-x-1/2 shadow-lg shadow-yellow-500/50">
+                {
+                    entries.map((entry, index) => (<JournalEntryCard key={entry.id} />))
+                }
+            </div>
+        </div>);
 }
 
 export default JournalTimeline;
