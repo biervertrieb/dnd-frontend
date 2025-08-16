a3bac0cfec9562bda3f7195f452a5a10bc79bdd9
biervertrieb <biervertrieb@googlemail.com>
2025-08-16 22:09:03 +0200

    first try background with tailwind css

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 const Layout = ({ children }) => {
     return (
-        <div className="layout">
+        <div className="min-h-screen bg-gradient-to-br from-amber-950 via-stone-900 to-amber-950">
             <Header />
             <Navigation />
             <main className="main">
