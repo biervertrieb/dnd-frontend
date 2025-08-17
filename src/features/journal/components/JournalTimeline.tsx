ec4324d729679f050c4e10a4b8384c101025e0dc
Basti <biervertrieb@googlemail.com>
2025-08-17 10:10:56 +0200

    give some props to entry card

--- src/features/journal/components/JournalTimeline.tsx
+++ src/features/journal/components/JournalTimeline.tsx
@@ -31,11 +31,14 @@ entries = [
 const JournalTimeline = () => {
     return (
         <div className="relative px-8">
-            <div className="absolute left-1/2 top-0 bottom-0 w-1 bg-gradient-to-b from-yellow-400 via-yellow-500 to-yellow-400 transform -translate-x-1/2 shadow-lg shadow-yellow-500/50">
-                {
-                    entries.map((entry, index) => (<JournalEntryCard key={entry.id} />))
-                }
-            </div>
+            <div className="absolute left-1/2 top-0 bottom-0 w-1 bg-gradient-to-b from-yellow-400 via-yellow-500 to-yellow-400 transform -translate-x-1/2 shadow-lg shadow-yellow-500/50"></div>
+            {
+                entries.map((entry, index) => (<JournalEntryCard
+                    key={entry.id}
+                    entry={entry}
+                    index={index}
+                />))
+            }
         </div>);
 }
 
