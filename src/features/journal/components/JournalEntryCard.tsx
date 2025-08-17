747e3e2bd65cab73fc0b4fa71a8d608942fc7698
Basti <biervertrieb@googlemail.com>
2025-08-17 13:06:54 +0200

    focus and mock snipped

--- src/features/journal/components/JournalEntryCard.tsx
+++ src/features/journal/components/JournalEntryCard.tsx
@@ -1,26 +1,37 @@
+import ReactMarkdown from "react-markdown";
 import type { JournalEntry } from "../types";
 
 type Props = {
-    entry: JournalEntry
+    entry: JournalEntry,
+    isFocus: boolean,
+    onClick: () => void
 }
 
-const JournalEntryCard = ({ entry }: Props) => {
+const JournalEntryCard = ({ entry, isFocus, onClick }: Props) => {
 
     return (
-        <article className="px-8 py-8 bg-gradient-to-br from-amber-50/95 to-amber-100/95">
+        <article onClick={onClick} className="px-8 py-8 bg-gradient-to-br from-amber-50/95 to-amber-100/95">
             <header>
                 <h3 className="text-2xl font-semibold text-amber-800 mb-1">Day {entry.day}: {entry.title}</h3>
             </header>
-            <div>
-                <textarea
-                    className="w-full border-2 rounded-lg px-4 py-4"
-                    value={entry.body}
-                    rows={8}
-                >
-                </textarea>
+            <div className="w-full px-4 py-4">
+                {isFocus && (
+                    <ReactMarkdown>
+                        {entry.body}
+                    </ReactMarkdown>
+                )}
+                {!isFocus && (
+                    <ReactMarkdown>
+                        {snip(entry.body)}
+                    </ReactMarkdown>
+                )}
             </div>
         </article>
     );
 }
 
 export default JournalEntryCard;
+
+function snip(text: string): string {
+    return "snip: " + text;
+}
