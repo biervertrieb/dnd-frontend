c5295110d2d88adc89c4f2558aff1262aaebadc3
biervertrieb <biervertrieb@googlemail.com>
2025-08-25 13:25:47 +0200

    added loading state

--- src/features/modals/CompendiumModal.tsx
+++ src/features/modals/CompendiumModal.tsx
@@ -3,11 +3,16 @@ import Modal from "./Modal";
 
 const CompendiumModal = () => {
     const showing = useAppStore((s) => s.showingCompendiumNote);
+    const loading = useAppStore((s) => s.compendiumLoading);
 
     if (!showing) return null;
     return (
         <Modal>
-            <div>Compendium Card coming soon...</div>
+            {loading ? (
+                <div>Loading....</div>
+            ) : (
+                <div>Compendium Card coming soon...</div>
+            )}
         </Modal>
     );
 };
