21b115bea73c03fec59210a5b6e51e2b14615752
Basti <biervertrieb@googlemail.com>
2025-09-15 16:18:43 +0200

    unused

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import JournalPage from "../features/journal/JournalPage";
